3d97a67a99113d23ba779a69118b356350362443
mizan21 <mizanorrahman398@gmail.com>
2023-06-15 06:01:40 +0600

    testing husky

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,12 @@
 import express, { Application, Request, Response } from 'express'
-import cors from "cors"
+import cors from 'cors'
 
-const app:Application = express()
+const app: Application = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-const port = 5000
-
-app.get('/', (req:Request, res:Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Worked successfully!')
 })
 
